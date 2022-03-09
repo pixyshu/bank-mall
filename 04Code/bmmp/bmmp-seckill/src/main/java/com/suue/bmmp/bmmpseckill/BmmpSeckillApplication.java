3f6cc6607a3d052d1e7940223cc0e198cad685f9
imcoco <1120132909@qq.com>
2022-03-09 17:03:10 +0800

    秒杀服务

---
+++ 04Code/bmmp/bmmp-seckill/src/main/java/com/suue/bmmp/bmmpseckill/BmmpSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.suue.bmmp.bmmpseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmmpSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmmpSeckillApplication.class, args);
+    }
+
+}
---
+++ 04Code/bmmp/bmmp-seckill/src/test/java/com/suue/bmmp/bmmpseckill/BmmpSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suue.bmmp.bmmpseckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmmpSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
